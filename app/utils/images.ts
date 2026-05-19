6cd15e5ea9f0385c44e8465a9c785a1555939c4c
Ronish <stharonish32@gmail.com>
2026-05-19 13:53:10 +0545

    update: images folder updated

--- app/utils/images.ts
+++ app/utils/images.ts
@@ -1,12 +1,12 @@
 export const IMAGES = {
   LEAF: "/images/plant_hero.png",
 
-  // HOME SECTION
-  HOME_HERO: "/images/home_hero.png",
+  // HERO SECTION
+  HOME_HERO: "/images/hero/home_hero.png",
 
-  // EXPERIENCE SECTION
-  EXPERIENCE_MAIN: "/images/experience_main.png",
-  EXPERIENCE_AROMA: "/images/experience_aroma.png",
-  EXPERIENCE_STEAM: "/images/experience_steam.png",
-  EXPERIENCE_FOOD: "/images/experience_food.png",
+  // HERO - EXPERIENCE SECTION
+  EXPERIENCE_MAIN: "/images/hero/experience_main.png",
+  EXPERIENCE_AROMA: "/images/hero/experience_aroma.png",
+  EXPERIENCE_STEAM: "/images/hero/experience_steam.png",
+  EXPERIENCE_FOOD: "/images/hero/experience_food.png",
 };
