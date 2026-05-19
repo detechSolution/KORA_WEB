d6f6a8a970fa79b28871c420bb17af5b6f00e551
Ronish <stharonish32@gmail.com>
2026-05-19 17:28:13 +0545

    update: button outline color updated

--- app/components/base/button.vue
+++ app/components/base/button.vue
@@ -37,7 +37,7 @@ const getStyles = computed(() => {
       case "solid":
         return "bg-primary-700 dark:bg-primary-700 hover:bg-primary-700/75 active:bg-primary-700/75 disabled:bg-primary-700 disabled:dark:bg-primary-700 text-white";
       case "outline":
-        return "border ring-0 text-secondary dark:text-white bg-transparent border-stone-300 hover:bg-stone-100 dark:hover:text-secondary disabled:bg-transparent";
+        return "border border-primary-700 dark:border-primary-700 hover:ring-0 text-primary-700 dark:text-primary-700 bg-primary/15 dark:bg-primary/15 hover:bg-primary/20 dark:hover:text-primary-700 disabled:bg-transparent";
       case "ghost":
         return "bg-transparent hover:bg-primary/10 disabled:bg-transparent";
       case "link":
